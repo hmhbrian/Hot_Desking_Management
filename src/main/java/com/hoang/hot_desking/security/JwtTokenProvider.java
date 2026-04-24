19cfa830e36c59aa93e5a936d47ee3a1a7f04a41
hmhbrian <hoanghm121103@gmail.com>
2026-04-24 11:12:42 +0700

    feat(security): implement JWT authentication and security filter chain

--- src/main/java/com/hoang/hot_desking/security/JwtTokenProvider.java
+++ src/main/java/com/hoang/hot_desking/security/JwtTokenProvider.java
@@ -1,4 +1,66 @@
 package com.hoang.hot_desking.security;
 
+import com.hoang.hot_desking.entity.User;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Slf4j
+@Component
 public class JwtTokenProvider {
+    @Value("${app.jwtSecret}")
+    private String jwtSecret;
+
+    @Value("${app.jwtExpirationInMs}")
+    private long jwtExpirationInMs;
+
+    //create jwt token
+    String generateToken(User user){
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + jwtExpirationInMs);
+
+        return Jwts.builder()
+                .setSubject(user.getId().toString())
+                .claim("email",user.getEmail())
+                .claim("role",user.getRole().name())
+                .setIssuedAt(new Date())
+                .setExpiration(expiryDate)
+                .signWith(Keys.hmacShaKeyFor(jwtSecret.getBytes()), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    //Validates the integrity and expiration of the given JWT token
+    public boolean validateToken(String authToken){
+        try{
+            Jwts.parserBuilder()
+                    .setSigningKey(Keys.hmacShaKeyFor(jwtSecret.getBytes()))
+                    .build()
+                    .parseClaimsJws(authToken);
+            return true;
+        }catch (MalformedJwtException ex){
+            log.error("Invalid JWT token strcture");
+        }catch(ExpiredJwtException ex){
+            log.error("JWT token has expired. Expiration date: {}", ex.getClaims().getExpiration());
+        }catch (IllegalArgumentException ex) {
+            log.error("JWT claims string is empty or null");
+        } catch (SignatureException ex) {
+            log.error("JWT signature verification failed");
+        }
+
+        return false;
+    }
+
+    //Extracts the User ID (subject) stored in the JWT payload
+    public String getUserIdFromJWT(String token){
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(Keys.hmacShaKeyFor(jwtSecret.getBytes()))
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.getSubject();
+    }
 }
