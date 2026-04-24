249b7e8b9134556ce2c1fd93983c96beb81aee17
hmhbrian <hoanghm121103@gmail.com>
2026-04-24 11:13:29 +0700

    feat(auth): integrate Google OAuth2 login with user persistence

--- src/main/java/com/hoang/hot_desking/security/CustomOAuth2UserService.java
+++ src/main/java/com/hoang/hot_desking/security/CustomOAuth2UserService.java
@@ -1,4 +1,54 @@
 package com.hoang.hot_desking.security;
 
-public class CustomOAuth2UserService {
+import com.hoang.hot_desking.entity.User;
+import com.hoang.hot_desking.entity.enums.UserRole;
+import com.hoang.hot_desking.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+    private final UserRepository userRepository;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        //lấy thông tin thô từ Google
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+
+        String email = oAuth2User.getAttribute("email");
+        String name = oAuth2User.getAttribute("name");
+        String picture = oAuth2User.getAttribute("picture");
+        String sub = oAuth2User.getAttribute("sub"); //googleId
+
+        log.info("Bắt đầu xử lý dữ liệu từ Google cho Email: {}", email);
+
+        userRepository.findByEmail(email)
+                .ifPresentOrElse(
+                        user -> {
+                            user.setFullName(name);
+                            user.setPictureUrl(picture);
+                            user.setGoogleId(sub);
+                            userRepository.save(user);
+                        },
+                        () -> {
+                            User newUser = User.builder()
+                                    .email(email)
+                                    .fullName(name)
+                                    .pictureUrl(picture)
+                                    .googleId(sub)
+                                    .role(UserRole.EMPLOYEE)
+                                    .enabled(true)
+                                    .build();
+                            userRepository.save(newUser);
+                        }
+                );
+        return oAuth2User;
+    }
 }
--- src/main/java/com/hoang/hot_desking/security/OAuth2Successhandler.java
+++ src/main/java/com/hoang/hot_desking/security/OAuth2Successhandler.java
@@ -1,4 +1,46 @@
 package com.hoang.hot_desking.security;
 
-public class OAuth2Successhandler {
+import com.hoang.hot_desking.entity.User;
+import com.hoang.hot_desking.repository.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class OAuth2Successhandler extends SimpleUrlAuthenticationSuccessHandler {
+    private final JwtTokenProvider tokenProvider;
+    private final UserRepository userRepository;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                        Authentication authentication) throws IOException {
+
+        DefaultOAuth2User oauthUser = (DefaultOAuth2User) authentication.getPrincipal();
+        String email = oauthUser.getAttribute("email");
+
+        // Tìm user vừa được CustomOAuth2UserService lưu vào DB
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        // Tạo Token
+        String token = tokenProvider.generateToken(user);
+        log.info("Đăng nhập thành công, đang tạo token cho email: {}", email);
+
+        // Trả Token về cho Client
+        String targetUrl = "http://localhost:8080/swagger-ui/index.html?token=" + token;
+        getRedirectStrategy().sendRedirect(request, response, targetUrl);
+
+    }
 }
